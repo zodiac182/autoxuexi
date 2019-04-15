2949b514ead6f4c4a1aa378593c264ac96beaa8e
zodiac182 <zodiac182@outlook.com>
2019-04-15 10:11:21 +0800

    v1.1

--- src/xuexi.py
+++ src/xuexi.py
@@ -67,7 +67,7 @@ class XUEXI:
         # chrome_options.add_argument('blink-settings=imagesEnabled=false') #不加载图片, 提升速度
         if config['background_process'] and self.use_Dingtalk:
             chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
-        self.driver = webdriver.Chrome('driver/chromedriver.exe', chrome_options=chrome_options)
+        self.driver = webdriver.Chrome('driver/chromedriver.exe', options=chrome_options)
         LOGGER.setLevel(logging.CRITICAL)
 
     '''
